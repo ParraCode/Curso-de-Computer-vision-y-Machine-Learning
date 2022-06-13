edbbe88cd674553753c0c6d32cd86deb0fd0356e
parracode <parrarodriguezmiguelangel@gmail.com>
2022-06-13 16:39:45 +0200

    Modulo2. Modulo1 terminado

--- apuntes/funciones_auxiliares/aux_01.py
+++ apuntes/funciones_auxiliares/aux_01.py
@@ -1,13 +1,18 @@
 import pandas as pd
 import plotly.express as px
 import plotly.figure_factory as ff
+import matplotlib.pyplot as plt
+
 
 def img_gray_plot (img):
+    """
+    Funcion para plotear un grafico de barrdas con el numero de
+    repeticiones que aparace cada tono de gris
+    """
     diccionario = {}
-
     intensidades = img.flatten()
     lista = intensidades.tolist()
-
+    
     for elemento in lista:
         clave = elemento
         valor = lista.count(clave)
@@ -19,6 +24,10 @@ def img_gray_plot (img):
     return fig.show()
 
 def img_rgb_plot (img):
+    """
+    Funcion para plotear un grafico de barrdas con el numero de
+    repeticiones que aparace cada tono de gris
+    """
     r = img[:,:,0].flatten()
     g = img[:,:,1].flatten()
     b = img[:,:,2].flatten()
@@ -34,3 +43,7 @@ def img_rgb_plot (img):
     fig = ff.create_distplot(hist_data, group_labels, show_hist=True, colors=colors)
     
     return fig.show()
+
+def show_img (img):
+    plt.imshow(img)
+    return plt.show()
